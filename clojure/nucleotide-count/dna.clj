f22ba0e702e553531ba71f84b5a968568b5a6f11
Axylos <robertdraketalley@gmail.com>
2014-04-11 03:14:26 -0400

    replace def and defns with let

--- clojure/nucleotide-count/dna.clj
+++ clojure/nucleotide-count/dna.clj
@@ -5,16 +5,15 @@
   (if (re-matches #"([^ACGTU])" (str nucleo))
     (throw (Exception. "invalid nucleotide"))
   )
-    
-  (def letters (seq string))
+
   (or (get (frequencies string) nucleo) 0)
 
 )
 
 (defn dna/nucleotide-counts [stuff]
   
-  (def nucs (hash-map \A 0 \C 0 \G 0 \T 0 ))
-  (merge nucs (frequencies stuff))
+  (let [nucs (hash-map \A 0 \C 0 \G 0 \T 0 )]
+  (merge nucs (frequencies stuff)))
   
 )
 
