55e3a69329210253216f4b3af8319d10cae1d081
Axylos <robertdraketalley@gmail.com>
2014-04-15 03:03:38 -0400

    refactor school

--- clojure/grade-school/school.clj
+++ clojure/grade-school/school.clj
@@ -2,17 +2,6 @@
 
 (defn add [db student grade]
   (merge-with concat db { grade  [student] }))
-  
-
-(def db {})
-(print (-> db
-    (add "Jeffrey" 5)
-    (add "Drake" 6)
-    (add "Mandy" 6)))
-
-  ;;(def db (merge-with concat  db {grade [student]})))
-
-
 
 (defn grade [db grade] 
   (or (db grade) []))
